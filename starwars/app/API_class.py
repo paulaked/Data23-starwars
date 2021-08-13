c1473f1562ae4ce16a2be1088071b24ef84b3cef
JDallimore <jdallimore@spartaglobal.com>
2021-08-13 11:04:52 +0100

    Updated API class with more functionality and added more tests

--- starwars/app/API_class.py
+++ starwars/app/API_class.py
@@ -5,17 +5,23 @@ class Api:
 
     def __init__(self, url: str):
         self.url = url
-        self.status_code = self.get().status_code
-        self.json = self.get().json()
+        self.status_code = self.get(self.url).status_code
+        self.json = self.get(self.url).json()
 
-    def get(self):
-        return requests.get(self.url)
+    # uses the get request to pull data using the SWAPI
+    def get(self, url: str):
+        return requests.get(url)
 
+    # used to access the results of an API request after adding an endpoint e.g. 'starships' or 'people'
     def results(self):
-        full_list = self.get().json()['results']
+        full_list = self.get(self.url).json()['results']
         url = self.url
-        while self.get().json()['next'] is not None:
-            self.url = self.get().json()['next']
-            full_list += self.get().json()['results']
+        while self.get(self.url).json()['next'] is not None:
+            self.url = self.get(self.url).json()['next']
+            full_list += self.get(self.url).json()['results']
         self.url = url
         return full_list
+
+    # returns the name of the person who matches the API url request
+    def pilot_name(self, url):
+        return self.get(url).json()['name']
--- starwars/tests/test_API_class.py
+++ starwars/tests/test_API_class.py
@@ -16,7 +16,7 @@ class ApiTests(unittest.TestCase):
         self.assertEqual(self.api_test.url, "https://swapi.dev/api/")
 
     def test_d_get(self):
-        self.assertEqual(str(self.api_test.get()), '<Response [200]>')
+        self.assertEqual(str(self.api_test.get(self.api_test.url)), '<Response [200]>')
 
     starship_test = Api("https://swapi.dev/api/starships/")
 
@@ -26,11 +26,14 @@ class ApiTests(unittest.TestCase):
     def test_f_starships_status(self):
         self.assertEqual(self.starship_test.status_code, 200)
 
-    def test_g_starships_results_type(self):
+    def test_g_starships_results(self):
         self.assertEqual(type(self.starship_test.results()), list)
-
-    def test_h_starships_results_length(self):
         self.assertEqual(len(self.starship_test.results()), 36)
+
+    def test_h_pilot_names(self):
+        self.assertEqual(self.starship_test.pilot_name('https://swapi.dev/api/people/13/'), 'Chewbacca')
+        self.assertEqual(self.starship_test.pilot_name('https://swapi.dev/api/people/15/'), 'Greedo')
+        self.assertEqual(self.starship_test.pilot_name('https://swapi.dev/api/people/51/'), 'Mace Windu')
 
     people_test = Api("https://swapi.dev/api/people/")
 
@@ -42,3 +45,4 @@ class ApiTests(unittest.TestCase):
 
     def test_k_people_results(self):
         self.assertEqual(len(self.people_test.results()), 82)
+
